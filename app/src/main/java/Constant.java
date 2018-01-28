71017f811571b17bbc24e2404fa2d9d940924e17
Krish <thekrish360@hotmail.com>
2018-01-27 23:23:42 -0500

    Added constants for league settings, functions to calculate averages, and team Ids

--- app/src/main/java/Constant.java
+++ app/src/main/java/Constant.java
@@ -5,6 +5,45 @@
 public class Constant {
 
     public enum teamID {
-        ()
+        Anaheim(3),
+        Arizona(6),
+        Boston(18),
+        Buffalo(21),
+        Calgary(4),
+        Carolina(27),
+        Chicago(9),
+        Colorado(8),
+        Columbus(24),
+        Dallas(10),
+        Detroit(16),
+        Edmonton(2),
+        Florida(17),
+        LosAngeles(7),
+        Minnesota(13),
+        Montreal(20),
+        NewJersey(29),
+        NewYorkIslanders(25),
+        NewYorkRangers(26),
+        Ottawa(15),
+        Philadelphia(28),
+        Pittsburg(23),
+        SaintLouis(14),
+        SanJose(5),
+        TampaBay(22),
+        Toronto(19),
+        Vancouver(1),
+        Vegas(35),
+        Washington(30),
+        Winnipeg(12);
+
+        public int iD;
+        teamID(int id){
+            iD = id;
+        }
+        public int getId(teamId team){
+            return team.iD;
+        }
     }
+
+
 }
--- app/src/main/java/LeagueSettings.java
+++ app/src/main/java/LeagueSettings.java
@@ -3,5 +3,30 @@
  */
 
 public class LeagueSettings {
-    
+
+    static int goals;
+    static int assists;
+    static int points;
+    static int pim;
+    static int ppg;
+    static int ppa;
+    static int ppp;
+    static int shg;
+    static int sha;
+    static int shp;
+    static int gwg;
+    static int fw;
+    static int fl;
+    static int hit;
+    static int blk;
+
+    static int gs;
+    static int win;
+    static int loss;
+    static int sho;
+    static int sa;
+    static int sv;
+    static int ga;
+    static int gaa;
+
 }
